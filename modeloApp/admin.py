2f553bbdce7f05b915acb8ca48ae922393ed77ff
Cristofher <cristofherrojascr1@gmail.com>
2023-10-03 09:22:16 -0300

    Se configura el archivo admin.py

--- modeloApp/admin.py
+++ modeloApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from modeloApp.models import Persona
 
 # Register your models here.
+
+#Desplegar atributos en el panel admin, y no ver solo un object.
+class PersonaAdmin(admin.ModelAdmin):
+    list_display = ['id','nombre','correo','telefono']
+
+
+admin.site.register(Persona, PersonaAdmin)
